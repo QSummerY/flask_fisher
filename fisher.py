8d7ebfb8d62aa4316fe014cbde7dbb398be456d1
qinxiayu <qinxiayu@douban.com>
2022-05-21 11:44:58 +0800

    update readme

--- fisher.py
+++ fisher.py
@@ -3,5 +3,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    # 启动文件
+    # entrypoint
     app.run(host='0.0.0.0', debug=app.config['DEBUG'], port=81, threaded=True)
